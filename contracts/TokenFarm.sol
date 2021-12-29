914edefd2cbc1bac2142632c2652d6b04fb3b865
RudyJakub <kubazewsi@gmail.com>
2021-12-29 20:50:14 +0100

    finished smart contract work

---
+++ contracts/TokenFarm.sol
@@ -0,0 +1,106 @@
+// stakeTokens
+// unstakeTokens
+// issueTokens
+// addAllowedTokens
+// getETHvalue
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract TokenFarm is Ownable {
+
+    mapping(address => mapping(address => uint256)) public stakingBalance;
+    mapping(address => uint256) public uniqueTokensStaked;
+    mapping(address => address) public tokenPriceFeedMapping;
+    address[] public stakers;
+    address[] public allowedTokens;
+    IERC20 public dappToken;
+
+    constructor(address _dappTokenAddress) public {
+        dappToken = IERC20(_dappTokenAddress);
+    }
+
+    function setPriceFeedContract(address _token, address _priceFeed) public {
+        tokenPriceFeedMapping[_token] = _priceFeed;
+    }
+
+    function issueTokens() public onlyOwner {
+        for (uint i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint256 userTotalValue = getUserTotalValue(recipient);
+            dappToken.transfer(recipient, userTotalValue);
+        }
+    }
+
+    function getUserTotalValue(address _user) public view returns (uint256) {
+        uint256 totalValue = 0;
+        require(uniqueTokensStaked[_user] > 0, "User has no tokens staked");
+        for (uint i = 0; i < allowedTokens.length; i++) {
+            totalValue += getUserSingleTokenValue(_user, allowedTokens[i]);
+        }
+        return totalValue;
+    }
+
+    function getUserSingleTokenValue(address _user, address _token) public view returns(uint256) {
+        if (uniqueTokensStaked[_user] <= 0) {
+            return 0;
+        }
+        (uint256 price, uint256 decimals) = getTokenValue(_token);
+        return (stakingBalance[_token][_user] * price) / (10 ** decimals);
+    }
+
+    function getTokenValue(address _token) public view returns (uint256, uint256) {
+        address priceFeedAddress = tokenPriceFeedMapping[_token];
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeedAddress);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        uint256 decimals = priceFeed.decimals();
+        return (uint256(price), decimals);
+    }
+
+    function stakeTokens(uint256 _amount, address _token) public {
+        require(_amount > 0, "Amount must be greater than 0");
+        require(tokenIsAllowed(_token), "Token is not allowed");
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        updateUniqueTokensStaked(msg.sender, _token);
+        stakingBalance[_token][msg.sender] = stakingBalance[_token][msg.sender] + _amount;
+        if (uniqueTokensStaked[msg.sender] == 1) {
+            stakers.push(msg.sender);
+        }
+    }
+
+    function unstakeTokens(address _token) public {
+        uint256 balance = stakingBalance[_token][msg.sender];
+        require(balance > 0, "User has no tokens staked");
+        IERC20(_token).transfer(msg.sender, balance);
+        stakingBalance[_token][msg.sender] = 0;
+        uniqueTokensStaked[msg.sender] = uniqueTokensStaked[msg.sender] - 1;
+        if (uniqueTokensStaked[msg.sender] == 0) {
+            stakers.remove(msg.sender);
+        }
+    }
+
+    function updateUniqueTokensStaked(address _user, address _token) internal {
+        if (stakingBalance[_token][_user] <= 0) {
+            uniqueTokensStaked[_user] = uniqueTokensStaked[_user] + 1;
+        }
+    }
+
+    function addAllowedTokens(address _token) public {
+        require(_token != address(0), "Token must not be the null address");
+        // require(allowedTokens.length < 10, "Too many allowed tokens");
+        allowedTokens.push(_token);
+    }
+
+    function tokenIsAllowed(address _token) public returns (bool) {
+        for (uint256 i = 0; i < allowedTokens.length; i++) {
+            if(allowedTokens[i] == _token) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
